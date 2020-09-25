d6831b89ae19eb707c4163ce86d2f243c812012b
kim0612 <roki03@naver.com>
2020-09-25 16:32:26 +0900

    1.0_Pug Compilation

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,26 @@
 import gulp from "gulp";
+import Gpug from "gulp-pug";
+import del from "del";
+
+const routes = {
+  delete : './build',
+  pug : {
+    src : './src/*.pug',
+    dest : './build'
+  }
+};
+
+const task_pug = () => 
+  gulp
+    .src(routes.pug.src)
+    .pipe(Gpug())
+    .pipe(gulp.dest(routes.pug.dest));
+
+const task_del = () => del(routes.delete);
+
+
+
+const prepare = gulp.series([task_del]);
+const assets = gulp.series([task_pug]);
+
+export const dev = gulp.series([prepare ,assets]);
