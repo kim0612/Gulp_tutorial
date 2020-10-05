f804cc8937729f6f564b97f9df94b34326c9c91b
kim0612 <roki03@naver.com>
2020-10-05 23:11:26 +0900

    2.0

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,8 @@
 import gulp from "gulp";
 import Gpug from "gulp-pug";
 import del from "del";
+import Gws from "gulp-webserver";
+
 
 const routes = {
   delete : './build',
@@ -10,17 +12,21 @@ const routes = {
   }
 };
 
+const task_del = () => del(routes.delete);
+
 const task_pug = () => 
   gulp
     .src(routes.pug.src)
     .pipe(Gpug())
     .pipe(gulp.dest(routes.pug.dest));
 
-const task_del = () => del(routes.delete);
-
+const task_webserver = () => 
+  gulp
+    .src('./build')
+    .pipe(Gws({livereload:true, open:true})); 
 
 
 const prepare = gulp.series([task_del]);
 const assets = gulp.series([task_pug]);
 
-export const dev = gulp.series([prepare ,assets]);
+export const dev = gulp.series([prepare ,assets, task_webserver]);
