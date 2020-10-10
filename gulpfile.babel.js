d78ac8e1332d7176e849a067b8d98295a5e733cf
kim0612 <roki03@naver.com>
2020-10-10 22:07:44 +0900

    3.0_Image optimization

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@ import gulp, { watch } from "gulp";
 import Gpug from "gulp-pug";
 import del from "del";
 import Gws from "gulp-webserver";
+import Gimg from "gulp-image";
 
 
 const routes = {
@@ -11,8 +12,13 @@ const routes = {
     dest : './build',
     watch : './src/**/*.pug'
   },
-  webserver : './build'
+  webserver : './build',
+  img : {
+    src : './src/img/*',
+    dest : './build/img'
+  }
 };
+
 
 const task_del = () => del(routes.delete);
 
@@ -27,12 +33,18 @@ const task_webserver = () =>
     .src(routes.webserver)
     .pipe(Gws({livereload:true, open:true})); 
 
-const task_watch = () =>
-  gulp
-    .watch(routes.pug.watch, task_pug);
+const task_watch = () => gulp.watch(routes.pug.watch, task_pug);
 
+const task_image = () =>
+  gulp
+    .src(routes.img.src)
+    .pipe(Gimg())
+    .pipe(gulp.dest(routes.img.dest));
+
+    
 const prepare = gulp.series([task_del]);
-const assets = gulp.series([task_pug]);
-const postDev = gulp.series([task_webserver,task_watch]);
+const assets = gulp.series([task_pug, task_image]);
+const postDev = gulp.parallel([task_webserver,task_watch]);
+
 
 export const dev = gulp.series([prepare ,assets, postDev]);
