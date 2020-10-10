afbeab42fe10a8b4c9241a3b0c6f9fdd839ab24b
kim0612 <roki03@naver.com>
2020-10-10 23:14:52 +0900

    4.0_SCSS Compilation

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,11 @@
-import gulp, { watch } from "gulp";
+import gulp, { dest, watch } from "gulp";
 import Gpug from "gulp-pug";
 import del from "del";
 import Gws from "gulp-webserver";
 import Gimg from "gulp-image";
+import Gsass from "gulp-sass";
+
+Gsass.compiler = require('node-sass');
 
 
 const routes = {
@@ -16,6 +19,11 @@ const routes = {
   img : {
     src : './src/img/*',
     dest : './build/img'
+  },
+  scss : {
+    src : './src/scss/style.scss',
+    dest : './build/css',
+    watch : './src/scss/style.scss'
   }
 };
 
@@ -28,22 +36,30 @@ const task_pug = () =>
     .pipe(Gpug())
     .pipe(gulp.dest(routes.pug.dest));
 
-const task_webserver = () => 
-  gulp
-    .src(routes.webserver)
-    .pipe(Gws({livereload:true, open:true})); 
-
-const task_watch = () => gulp.watch(routes.pug.watch, task_pug);
-
 const task_image = () =>
   gulp
     .src(routes.img.src)
     .pipe(Gimg())
     .pipe(gulp.dest(routes.img.dest));
 
+const task_scss = () =>
+  gulp
+    .src(routes.scss.src)
+    .pipe(Gsass().on('error', Gsass.logError))
+    .pipe(dest(routes.scss.dest));
     
+const task_webserver = () => 
+  gulp
+    .src(routes.webserver)
+    .pipe(Gws({livereload:true, open:true})); 
+
+const task_watch = () => 
+  gulp.watch(routes.pug.watch, task_pug);
+  gulp.watch(routes.scss.watch, task_scss);
+
+
 const prepare = gulp.series([task_del]);
-const assets = gulp.series([task_pug, task_image]);
+const assets = gulp.series([task_pug, task_image, task_scss]);
 const postDev = gulp.parallel([task_webserver,task_watch]);
 
 
